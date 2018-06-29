1920acb697d1077e34880e3729c61e65f40afb73
Brandon Lindsey <blindsey@seattlegoodwill.org>
2018-06-28 19:13:30 -0700

    Updated

--- Class 6.28.18.sql
+++ Class 6.28.18.sql
@@ -1,5 +1,17 @@
-﻿--Class 6.28
+﻿--Class 6.28.18
 
+--Code Along
 SELECT userid, referred_by
 FROM da_readychef.users u 
 LEFT JOIN da_readychef.referrals r ON u.userid = r.referred
+
+--LEFT OUTER JOIN 
+SELECT DISTINCT COUNT (customers.id)
+FROM da_sales.customers 
+LEFT JOIN da_sales.purchases ON customers.id = custid
+
+--JOINS with more than two tables 
+SELECT customers.name, products.name
+FROM da_sales.purchases 
+LEFT JOIN da_sales.customers ON custid = customers.id
+LEFT JOIN da_sales.products ON prodid = products.id
